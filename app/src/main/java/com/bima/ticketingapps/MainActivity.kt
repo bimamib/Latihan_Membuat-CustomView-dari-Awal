10db4c6c3a247efaae96abd22f6d59a363e285a3
bimamib <bimaprast1601@gmail.com>
2023-10-02 18:20:13 +0700

    menghubungkan SeatsView dan Button

--- app/src/main/java/com/bima/ticketingapps/MainActivity.kt
+++ app/src/main/java/com/bima/ticketingapps/MainActivity.kt
@@ -2,10 +2,14 @@ package com.bima.ticketingapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val seatView = findViewById<SeatsView>(R.id.seatsView)
+        val button = findViewById<Button>(R.id.finishButton)
     }
 }
