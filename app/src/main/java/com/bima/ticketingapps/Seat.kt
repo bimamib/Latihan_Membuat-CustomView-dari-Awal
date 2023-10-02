f2cac2b1fa349c3f64e0be9d69255dc325cb37b5
bimamib <bimaprast1601@gmail.com>
2023-10-02 09:42:47 +0700

    membuat array bernama seats

--- app/src/main/java/com/bima/ticketingapps/Seat.kt
+++ app/src/main/java/com/bima/ticketingapps/Seat.kt
@@ -7,3 +7,14 @@ data class Seat(
     var name: String,
     var isBooked: Boolean
 )
+
+private val seats: ArrayList<Seat> = arrayListOf(
+    Seat(id = 1, name = "A1", isBooked = false),
+    Seat(id = 2, name = "A2", isBooked = false),
+    Seat(id = 3, name = "B1", isBooked = false),
+    Seat(id = 4, name = "A4", isBooked = false),
+    Seat(id = 5, name = "C1", isBooked = false),
+    Seat(id = 6, name = "C2", isBooked = false),
+    Seat(id = 7, name = "D1", isBooked = false),
+    Seat(id = 8, name = "D2", isBooked = false),
+)
